8f7f3d3427991951a06e834561b0e34f0d059f35
Linn-it <linnaunghtetit@gmail.com>
2023-05-22 23:14:00 +0630

    #13 Authentication Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import Home from './pages/home/Home';
 import { Routes, Route } from 'react-router';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={< Login/>} />
       </Routes>
     </div>
   );
